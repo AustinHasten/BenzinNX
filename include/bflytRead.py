e297a20a66cc4542fb03ae594595472ccffe52d0
diddy81 <diddyman00@gmail.com>
2015-02-13 10:57:54 +0000

    added more sections
    added all the bflyt sections i could find and skipped reading them
    also skipped mat1 till i can work more out about it

--- include/bflytRead.py
+++ include/bflytRead.py
@@ -27,6 +27,30 @@ class ReadBflyt(object):
 			self.fnl1section(data, pos)
 		elif magic == "mat1":
 			self.mat1section(data, pos)
+		elif magic == "pan1":
+			self.pan1section(data, pos)
+		elif magic == "pas1":
+			self.pas1section(data, pos)
+		elif magic == "pic1":
+			self.pic1section(data, pos)
+		elif magic == "txt1":
+			self.txt1section(data, pos)
+		elif magic == "wnd1":
+			self.wnd1section(data, pos)
+		elif magic == "bnd1":
+			self.bnd1section(data, pos)
+		elif magic == "prt1":
+			self.prt1section(data, pos)
+		elif magic == "pae1":
+			self.pae1section(data, pos)
+		elif magic == "grp1":
+			self.grp1section(data, pos)
+		elif magic == "grs1":
+			self.grs1section(data, pos)
+		elif magic == "gre1":
+			self.gre1section(data, pos)
+		elif magic == "cnt1":
+			self.cnt1section(data, pos)
 		else:
 			print("Done")
 			#sys.exit(1)
@@ -53,8 +77,6 @@ class ReadBflyt(object):
 		self.newroot = etree.SubElement(self.root, "version", Number=str(version))
 		self.checkheader(data, pos)	
 		
-		
-		
 	def lyt1section(self, data, pos):
 		lyt1magic, lyt1length, pos = self.ReadMagic(data,pos)			# read magic & section length
 		drawnFromMiddle = RT.uint8(data, pos);pos += 1					# drawn from middle of the screen
@@ -74,8 +96,7 @@ class ReadBflyt(object):
 		etree.SubElement(tag, "filename").text = str(filename)
 		self.checkheader(data, pos)	
 		#self.debugfile(filename)
-		
-		
+				
 	def fnl1section(self, data, pos):
 		fnl1magic, fnl1length, pos = self.ReadMagic(data,pos)				# read magic & section length
 		NumFonts = RT.uint16(data, pos);pos += 2							# number of textures in section
@@ -96,9 +117,6 @@ class ReadBflyt(object):
 		padd = endoffontlist - startoffontlist
 		pad = RT.by4(padd) - padd
 		pos += pad
-	
-	
-	
 	
 	def txl1section(self, data, pos):
 		txl1magic, txl1length, pos = self.ReadMagic(data,pos)				# read magic & section length
@@ -121,91 +139,202 @@ class ReadBflyt(object):
 		pad = RT.by4(padd) - padd
 		pos += pad
 		self.checkheader(data, pos)	
-		
-		
-		
+			
 	def mat1section(self, data, pos):
 		StartPos = pos
 		mat1magic, mat1length, pos = self.ReadMagic(data,pos)				# read magic & section length
+		
 		NumMaterials = RT.uint16(data, pos);pos += 2						# number of materials in section
 		SizeHeader = RT.uint16(data, pos);pos += 2							# Offsetree to list start. Always zero
 		
 		tag = etree.SubElement(self.newroot, "tag", type="mat1")
 		
-		MaterialOffsetree = []
-		while len(MaterialOffsetree) < NumMaterials:
-			MaterialOffsetree.append(RT.uint32(data, pos));pos += 4
+		pos = StartPos + mat1length # debug skip section
+		# MaterialOffsetree = []
+		# while len(MaterialOffsetree) < NumMaterials:
+			# MaterialOffsetree.append(RT.uint32(data, pos));pos += 4
 			
-		i = 0
+		# i = 0
 		# while i < NumMaterials:
-		MatName = RT.getstr(data[pos:]);pos += 28		
-		entries = etree.SubElement(tag, "entries", name=MatName)
-		colors = etree.SubElement(entries, "colors" )
-		fore_color = []
-		while len(fore_color) < 4:
-			fore_color.append(RT.uint8(data, pos));pos += 1
-		colorfore = etree.SubElement(colors, "forecolor")
-		colorfore.attrib['R'] = str(fore_color[0])
-		colorfore.attrib['G'] = str(fore_color[1])
-		colorfore.attrib['B'] = str(fore_color[2])
-		colorfore.attrib['A'] = str(fore_color[3])
-		back_color = []
-		while len(back_color) < 4:
-			back_color.append(RT.uint8(data, pos));pos += 1
-		colorback = etree.SubElement(colors, "backcolor")
-		colorback.attrib['R'] = str(back_color[0])
-		colorback.attrib['G'] = str(back_color[1])
-		colorback.attrib['B'] = str(back_color[2])
-		colorback.attrib['A'] = str(back_color[3])
-		colorReg3 = []
-		while len(colorReg3) < 4:
-			colorReg3.append(RT.uint8(data, pos));pos += 1
-		reg3color = etree.SubElement(colors, "colorReg3")
-		reg3color.attrib['R'] = str(colorReg3[0])
-		reg3color.attrib['G'] = str(colorReg3[1])
-		reg3color.attrib['B'] = str(colorReg3[2])
-		reg3color.attrib['A'] = str(colorReg3[3])
-		tev_k1 = []
-		while len(tev_k1) < 4:
-			tev_k1.append(RT.uint8(data, pos));pos += 1
-		k1tev = etree.SubElement(colors, "tev_k1")
-		k1tev.attrib['R'] = str(tev_k1[0])
-		k1tev.attrib['G'] = str(tev_k1[1])
-		k1tev.attrib['B'] = str(tev_k1[2])
-		k1tev.attrib['A'] = str(tev_k1[3])
-		tev_k2 = []
-		while len(tev_k2) < 4:
-			tev_k2.append(RT.uint8(data, pos));pos += 1
-		k2tev = etree.SubElement(colors, "tev_k2")
-		k2tev.attrib['R'] = str(tev_k2[0])
-		k2tev.attrib['G'] = str(tev_k2[1])
-		k2tev.attrib['B'] = str(tev_k2[2])
-		k2tev.attrib['A'] = str(tev_k2[3])
-		tev_k3 = []
-		while len(tev_k3) < 4:
-			tev_k3.append(RT.uint8(data, pos));pos += 1
-		k3tev = etree.SubElement(colors, "tev_k3")
-		k3tev.attrib['R'] = str(tev_k3[0])
-		k3tev.attrib['G'] = str(tev_k3[1])
-		k3tev.attrib['B'] = str(tev_k3[2])
-		k3tev.attrib['A'] = str(tev_k3[3])
-		tev_k4 = []
-		while len(tev_k4) < 4:
-			tev_k4.append(RT.uint8(data, pos));pos += 1
-		k4tev = etree.SubElement(colors, "tev_k4")
-		k4tev.attrib['R'] = str(tev_k4[0])
-		k4tev.attrib['G'] = str(tev_k4[1])
-		k4tev.attrib['B'] = str(tev_k4[2])
-		k4tev.attrib['A'] = str(tev_k4[3])
+		# MatName = RT.getstr(data[pos:]);pos += 28		
+		# entries = etree.SubElement(tag, "entries", name=MatName)
+		# colors = etree.SubElement(entries, "colors" )
+		# fore_color = []
+		# while len(fore_color) < 4:
+			# fore_color.append(RT.uint8(data, pos));pos += 1
+		# colorfore = etree.SubElement(colors, "forecolor")
+		# colorfore.attrib['R'] = str(fore_color[0])
+		# colorfore.attrib['G'] = str(fore_color[1])
+		# colorfore.attrib['B'] = str(fore_color[2])
+		# colorfore.attrib['A'] = str(fore_color[3])
+		# back_color = []
+		# while len(back_color) < 4:
+			# back_color.append(RT.uint8(data, pos));pos += 1
+		# colorback = etree.SubElement(colors, "backcolor")
+		# colorback.attrib['R'] = str(back_color[0])
+		# colorback.attrib['G'] = str(back_color[1])
+		# colorback.attrib['B'] = str(back_color[2])
+		# colorback.attrib['A'] = str(back_color[3])
+		# colorReg3 = []
+		# while len(colorReg3) < 4:
+			# colorReg3.append(RT.uint8(data, pos));pos += 1
+		# reg3color = etree.SubElement(colors, "colorReg3")
+		# reg3color.attrib['R'] = str(colorReg3[0])
+		# reg3color.attrib['G'] = str(colorReg3[1])
+		# reg3color.attrib['B'] = str(colorReg3[2])
+		# reg3color.attrib['A'] = str(colorReg3[3])
+		# tev_k1 = []
+		# while len(tev_k1) < 4:
+			# tev_k1.append(RT.uint8(data, pos));pos += 1
+		# k1tev = etree.SubElement(colors, "tev_k1")
+		# k1tev.attrib['R'] = str(tev_k1[0])
+		# k1tev.attrib['G'] = str(tev_k1[1])
+		# k1tev.attrib['B'] = str(tev_k1[2])
+		# k1tev.attrib['A'] = str(tev_k1[3])
+		# tev_k2 = []
+		# while len(tev_k2) < 4:
+			# tev_k2.append(RT.uint8(data, pos));pos += 1
+		# k2tev = etree.SubElement(colors, "tev_k2")
+		# k2tev.attrib['R'] = str(tev_k2[0])
+		# k2tev.attrib['G'] = str(tev_k2[1])
+		# k2tev.attrib['B'] = str(tev_k2[2])
+		# k2tev.attrib['A'] = str(tev_k2[3])
+		# tev_k3 = []
+		# while len(tev_k3) < 4:
+			# tev_k3.append(RT.uint8(data, pos));pos += 1
+		# k3tev = etree.SubElement(colors, "tev_k3")
+		# k3tev.attrib['R'] = str(tev_k3[0])
+		# k3tev.attrib['G'] = str(tev_k3[1])
+		# k3tev.attrib['B'] = str(tev_k3[2])
+		# k3tev.attrib['A'] = str(tev_k3[3])
+		# tev_k4 = []
+		# while len(tev_k4) < 4:
+			# tev_k4.append(RT.uint8(data, pos));pos += 1
+		# k4tev = etree.SubElement(colors, "tev_k4")
+		# k4tev.attrib['R'] = str(tev_k4[0])
+		# k4tev.attrib['G'] = str(tev_k4[1])
+		# k4tev.attrib['B'] = str(tev_k4[2])
+		# k4tev.attrib['A'] = str(tev_k4[3])
 		
 		
 		
-		i += 1
+		# i += 1
 		#-------------------------------------------------------------------------------------------------
 		
-		print hex(pos)
+		#print hex(pos)
+		self.checkheader(data, pos)	
 			
 			
+	def pan1section(self, data, pos):
+		StartPos = pos
+		pan1magic, pan1length, pos = self.ReadMagic(data,pos)				# read magic & section length
+		
+		tag = etree.SubElement(self.newroot, "tag", type="pan1")
+		
+		pos = StartPos + pan1length # debug skip section
+		self.checkheader(data, pos)	
+		
+	def pas1section(self, data, pos):
+		StartPos = pos
+		pas1magic, pas1length, pos = self.ReadMagic(data,pos)				# read magic & section length
+		
+		tag = etree.SubElement(self.newroot, "tag", type="pas1")
+		
+		pos = StartPos + pas1length # debug skip section
+		self.checkheader(data, pos)	
+		
+	def pic1section(self, data, pos):
+		StartPos = pos
+		pic1magic, pic1length, pos = self.ReadMagic(data,pos)				# read magic & section length
+		
+		tag = etree.SubElement(self.newroot, "tag", type="pic1")
+		
+		pos = StartPos + pic1length # debug skip section
+		self.checkheader(data, pos)	
+		
+	def txt1section(self, data, pos):
+		StartPos = pos
+		txt1magic, txt1length, pos = self.ReadMagic(data,pos)				# read magic & section length
+		
+		tag = etree.SubElement(self.newroot, "tag", type="txt1")
+		
+		pos = StartPos + txt1length # debug skip section
+		self.checkheader(data, pos)			
+		
+	def wnd1section(self, data, pos):
+		StartPos = pos
+		wnd1magic, wnd1length, pos = self.ReadMagic(data,pos)				# read magic & section length
+		
+		tag = etree.SubElement(self.newroot, "tag", type="wnd1")
+		
+		pos = StartPos + wnd1length # debug skip section
+		self.checkheader(data, pos)	
+		
+	def bnd1section(self, data, pos):
+		StartPos = pos
+		bnd1magic, bnd1length, pos = self.ReadMagic(data,pos)				# read magic & section length
+		
+		tag = etree.SubElement(self.newroot, "tag", type="bnd1")
+		
+		pos = StartPos + bnd1length # debug skip section
+		self.checkheader(data, pos)	
+		
+	def prt1section(self, data, pos):
+		StartPos = pos
+		prt1magic, prt1length, pos = self.ReadMagic(data,pos)				# read magic & section length
+		
+		tag = etree.SubElement(self.newroot, "tag", type="prt1")
+		
+		pos = StartPos + prt1length # debug skip section
+		self.checkheader(data, pos)	
+		
+	def pae1section(self, data, pos):
+		StartPos = pos
+		pae1magic, pae1length, pos = self.ReadMagic(data,pos)				# read magic & section length
+		
+		tag = etree.SubElement(self.newroot, "tag", type="pae1")
+		
+		pos = StartPos + pae1length # debug skip section
+		self.checkheader(data, pos)	
+		
+	def grp1section(self, data, pos):
+		StartPos = pos
+		grp1magic, grp1length, pos = self.ReadMagic(data,pos)				# read magic & section length
+		
+		tag = etree.SubElement(self.newroot, "tag", type="grp1")
+		
+		pos = StartPos + grp1length # debug skip section
+		self.checkheader(data, pos)	
+		
+	def grs1section(self, data, pos):
+		StartPos = pos
+		grs1magic, grs1length, pos = self.ReadMagic(data,pos)				# read magic & section length
+		
+		tag = etree.SubElement(self.newroot, "tag", type="grs1")
+		
+		pos = StartPos + grs1length # debug skip section
+		self.checkheader(data, pos)	
+		
+	def gre1section(self, data, pos):
+		StartPos = pos
+		gre1magic, gre1length, pos = self.ReadMagic(data,pos)				# read magic & section length
+		
+		tag = etree.SubElement(self.newroot, "tag", type="gre1")
+		
+		pos = StartPos + gre1length # debug skip section
+		self.checkheader(data, pos)	
+				
+	def cnt1section(self, data, pos):
+		StartPos = pos
+		cnt1magic, cnt1length, pos = self.ReadMagic(data,pos)				# read magic & section length
+		
+		tag = etree.SubElement(self.newroot, "tag", type="cnt1")
+		
+		pos = StartPos + cnt1length # debug skip section
+		self.checkheader(data, pos)	
+		
+		
+		
 		
 	
 	
@@ -219,10 +348,3 @@ class ReadBflyt(object):
 			dirpath.write(data)
 			
 			
-			
-			
-				
-		# crap = struct.pack(">%ds4HI2H"%RT.by4(len(bflytmagic)),bflytmagic,endian,lyt_offsetree,version,pad1,filesize,sections,pad2)
-		# print crap
-		# self.debugfile(crap)
-		
