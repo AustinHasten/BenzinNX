52284c49c6619c45d850f51049974738d791cf55
Austin Hasten <austinhasten@live.com>
2018-11-15 16:58:19 -0600

    Remove .bflyt types from types.py.

--- include/types.py
+++ include/types.py
@@ -1,19 +1,5 @@
 from collections import namedtuple
 
-wraps = ["NEAR_CLAMP", "NEAR_REPEAT", "NEAR_MIRROR", "GX2_MIRROR_ONCE", "CLAMP", "REPEAT", "MIRROR", "GX2_MIRROR_ONCE_BORDER" ]
-originX = ["Center", "Left", "Right"]
-originY = ["Center", "Up", "Down"]
-MappingTypes = ["UVMapping", "", "", "OrthogonalProjection", "PaneBasedProjection"]
-BlendTypes = ["Max", "Min"]
-ColorBlendTypes = ["Overwrite", "Multiplicative", "Additive", "Exclusion", "4", "Subtractive", "ColorDodge", "ColorBurn", "Overlay", "Indirect", "BlendIndirect", "EachIndirect"]
-AlphaTestCondition = ["Never", "Less", "LessEqual", "Equal", "NotEqual", "GreaterEqual", "Greater", "Always"]
-BlendCalc = ["0", "1", "FBColor", "1-FBColor", "PixelAlpha", "1-PixelAlpha", "FBAlpha", "1-FBAlpha", "PixelColor", "1-PixelColor"]
-BlendCalcOp = ["0", "Add", "Subtract", "ReverseSubtract", "Min", "Max"]
-LogicalCalcOp = ["None", "NoOp", "Clear", "Set", "Copy", "InvCopy", "Inv", "And", "Nand", "Or", "Nor", "Xor", "Equiv", "RevAnd", "InvAnd", "RevOr", "InvOr"]
-ProjectionMappingTypes = ["Standard", "EntireLayout", "2", "3", "PaneRandSProjection", "5", "6"]
-TextAlign = ["NA", "Left", "Center", "Right"]
-
-#anim types
 typedict = {
     'FLPA': ["Xtrans", "Ytrans", "Ztrans", "Xrotate", "Yrotate", "Zrotate", "Xscale", "Yscale", "Xsize", "Ysize"],
     'FLVI': ["visible"],
