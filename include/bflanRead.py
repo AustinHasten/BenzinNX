36256466b53f988c18305cf669003a542d906d7e
Austin Hasten <austinhasten@live.com>
2018-11-16 19:29:44 -0600

    Refactor reading of pai section.

--- include/bflanRead.py
+++ include/bflanRead.py
@@ -57,8 +57,7 @@ class ReadBflan(object):
         strngs2 = objectify.SubElement(tag, 'AnimatedGroups')
         self.position = pat_start_pos + pat.secondsoffset
         for _ in range(pat.numseconds):
-            group_name = self.unpackkvarstr()
-            objectify.SubElement(strngs2, 'Groupname')._setText(str(group_name))
+            objectify.SubElement(strngs2, 'Groupname')._setText(self.unpackkvarstr())
 
         self.read_header()
 
@@ -73,7 +72,7 @@ class ReadBflan(object):
         self.position = pat_start_pos + pat.secondsoffset
         for _ in range(pat.numseconds):
             group_name = self.unpackkvarstr()
-            objectify.SubElement(strngs2, 'Groupname')._setText(str(group_name))
+            objectify.SubElement(strngs2, 'Groupname')._setText(group_name)
         
     def read_pai(self):
         pai_start_pos = self.position - 4
@@ -86,50 +85,52 @@ class ReadBflan(object):
         timgs_offsets = [ self.unpackk('I') for _ in range(pai.numtimgs) ]
         for timgs_offset in timgs_offsets:
             self.position = pai_start_pos + 20 + timgs_offset
-            timgs = objectify.SubElement(tag, 'timg')
-            timgs.attrib['name'] = self.unpackkvarstr()
+            timgs = objectify.SubElement(tag, 'timg', name=self.unpackkvarstr())
 
         self.position = pai_start_pos + pai.entryoffset
         pane_offsets = [ self.unpackk('I') for _ in range(pai.numentries) ]
         for pane_offset in pane_offsets:
-            self.position = pai_start_pos + pane_offset
-            pane = objectify.SubElement(tag, 'pane')
-            name = self.unpackkvarstr()
-            entry = types.entry_tup(*self.unpackk('BBH'))
+            self.read_pane(pai_start_pos, pane_offset)
 
-            pane.attrib['name'] = name
-            pane.attrib['type'] = str(entry.is_material)
+    def read_pane(self, pai_start_pos, pane_offset):
+        self.position = pai_start_pos + pane_offset
+        name = self.unpackkvarstr()
+        entry = types.entry_tup(*self.unpackk('BBH'))
+        pane = objectify.SubElement(tag, 'pane', name=name, type=str(entry.is_material))
 
-            tag_offsets = [ self.unpackk('I') for _ in range(entry.num_tags) ]
-            for tag_offset in tag_offsets:
-                self.position = pai_start_pos + pane_offset + tag_offset
-                typetree = objectify.SubElement(pane, 'tag')
-                tag_start_pos = self.position
-                tag_type = self.unpackkstr(4)
-                # TODO Handle this more elegantly if possible.
-                if tag_type == ',\x00\x00\x00':
-                    tag_type = self.unpackkstr(4)
-                typetree.attrib['type'] = tag_type
-                entry_count = self.unpackk('B')
-                pad = self.unpackk('3B')
-                    
-                tag_data_offsets = [ self.unpackk('I') for _ in range(entry_count) ]
-                for tag_data_offset in tag_data_offsets:
-                    self.position = tag_start_pos + tag_data_offset
-                    tag_data = types.tag_data_tup(*self.unpackk('BBHHHI'))
-                    try:
-                        typename = types.typedict[tag_type][tag_data.type2]
-                    except KeyError:
-                        typename = str(tag_data.type2)
+        tag_offsets = [ self.unpackk('I') for _ in range(entry.num_tags) ]
+        for tag_offset in tag_offsets:
+            self.read_tag(pai_start_pos, pane_offset, tag_offset)
 
-                    entry = objectify.SubElement(typetree, 'entry')
-                    entry.attrib['type1'] = str(tag_data.type1)
-                    entry.attrib['type2'] = typename
+    def read_tag(self, pai_start_pos, pane_offset, tag_offset):
+        self.position = pai_start_pos + pane_offset + tag_offset
+        typetree = objectify.SubElement(pane, 'tag')
+        tag_start_pos = self.position
+        tag_type = self.unpackkstr(4)
+        # TODO Handle this more elegantly if possible.
+        if tag_type == ',\x00\x00\x00':
+            tag_type = self.unpackkstr(4)
+        typetree.attrib['type'] = tag_type
+        entry_count = self.unpackk('B')
+        pad = self.unpackk('3B')
+            
+        tag_data_offsets = [ self.unpackk('I') for _ in range(entry_count) ]
+        for tag_data_offset in tag_data_offsets:
+            self.read_tag_data(tag_start_pos, tag_data_offset)
 
-                    if tag_data.datatype == 2:
-                        self.triplet(tag_data.coordcount, entry)
-                    elif tag_data.datatype == 1:
-                        self.pair(tag_data.coordcount, entry)
+    def read_tag_data(self, tag_start_pos, tag_data_offset):
+        self.position = tag_start_pos + tag_data_offset
+        tag_data = types.tag_data_tup(*self.unpackk('BBHHHI'))
+        try:
+            typename = types.typedict[tag_type][tag_data.type2]
+        except KeyError:
+            typename = str(tag_data.type2)
+
+        entry = objectify.SubElement(typetree, 'entry', type1=str(tag_data.type1), type2=typename)
+        if tag_data.datatype == 2:
+            self.triplet(tag_data.coordcount, entry)
+        elif tag_data.datatype == 1:
+            self.pair(tag_data.coordcount, entry)
     
     def triplet(self, count, entry):
         for _ in range(count):
