aeac1acbf7977b9d712ddfad87160b756dabf3d6
Austin Hasten <austinhasten@live.com>
2018-11-29 16:17:00 -0600

    Another rewrite.

--- include/bflanRead.py
+++ include/bflanRead.py
@@ -6,11 +6,37 @@ from struct import unpack_from, calcsize
 
 class ReadBflan(object):
     def __init__(self, data, output):
-        self.root = objectify.Element('xmflan')
-        self.position = 0
         self.data = data
-        self.read_header()
-        etree.ElementTree(self.root).write(output, pretty_print=True)
+        SE = objectify.SubElement
+        self.position = 0
+        header = self.read_header()
+        pat = self.read_pat()
+        pai = self.read_pai()
+
+        rootxml = objectify.Element('xmflan')
+        versionxml = SE(rootxml, 'version', Number=str(header['version']))
+        patxml = SE(versionxml, 'tag', type='pat1')
+        patxml.AnimOrder = pat['animorder']
+        patxml.StartOfFile = pat['start']
+        patxml.EndOfFile = pat['end']
+        patxml.ChildBinding = pat['childbinding']
+        patxml.First = pat['first']
+        AnimatedGroups = SE(patxml, 'AnimatedGroups')
+        AnimatedGroups.Groupname = pat['groupnames']
+        paixml = SE(versionxml, 'tag', type='pai1')
+        paixml.attrib['framesize'] = str(pai['framesize'])
+        paixml.attrib['flags'] = str(pai['flags'])
+        for timgname in pai['timgnames']:
+            SE(paixml, 'timg', name=timgname)
+        for pane in pai['panes']:
+            panexml = SE(paixml, 'pane', name=pane['name'], type=str(pane['is_material']))
+            for tag in pane['tags']:
+                tagxml = SE(panexml, 'tag', type=tag['type'])
+                for data in tag['datas']:
+                    entryxml = SE(tagxml, 'entry', type1=str(data['type1']), type2=data['typename'])
+                    for x in data['content']:
+                        entryxml.append(x)
+        etree.ElementTree(rootxml).write(output, pretty_print=True)
         print("File converted.")
 
     def unpackk(self, formstr, increment=True):
@@ -29,102 +55,108 @@ class ReadBflan(object):
         return string
 
     def read_header(self):
-        magic = self.unpackkstr(4)
-        routing = {'FLAN':self.read_bflanheader, 'pat1':self.read_pat, 'pai1':self.read_pai}
-        try:
-            routing[magic]()
-        except KeyError:
-            print(f"No code for {magic} section at {self.position}.")
-        
-    def read_bflanheader(self):
-        bflanheader = types.bflanheader_tup(*self.unpackk('HHHHIHH'))
-        self.newroot = objectify.SubElement(self.root, 'version', Number=str(bflanheader.version))
-        self.read_header()
-        
+        header = dict()
+        header['magic'] = self.unpackkstr(4)
+        header['endian'] = self.unpackk('H')
+        header['firstsectionoffsetree'] = self.unpackk('H')
+        header['version'] = self.unpackk('H')
+        header['pad1'] = self.unpackk('H')
+        header['filesize'] = self.unpackk('I')
+        header['sections'] = self.unpackk('H')
+        header['pad2'] = self.unpackk('H')
+        return header
+
     def read_pat(self):
-        pat_start_pos = self.position - 4
-        pat = types.pat_tup(*self.unpackk('IHHIIHHBBH'))
+        pat = dict()
+        pat['start_pos'] = self.position
+        pat['magic'] = self.unpackkstr(4)
+        pat['length'] = self.unpackk('I')
+        pat['animorder'] = self.unpackk('H')
+        pat['numseconds'] = self.unpackk('H')
+        pat['firstoffset'] = self.unpackk('I')
+        pat['secondsoffset'] = self.unpackk('I')
+        pat['start'] = self.unpackk('H')
+        pat['end'] = self.unpackk('H')
+        pat['childbinding'] = self.unpackk('B')
+        pat['pad'] = self.unpackk('B')
+        pat['pad1'] = self.unpackk('H')
+        self.position = pat['start_pos'] + pat['firstoffset']
+        pat['first'] = self.unpackkvarstr()
+        self.position = pat['start_pos'] + pat['secondsoffset']
+        pat['groupnames'] = [ self.unpackkvarstr() for _ in range(pat['numseconds']) ]
+        return pat
 
-        self.position = pat_start_pos + pat.firstoffset
-        first = self.unpackkvarstr()
-        
-        tag = objectify.SubElement(self.newroot, 'tag', type='pat1')
-        tag.AnimOrder = pat.animorder
-        tag.StartOfFile = pat.start
-        tag.EndOfFile = pat.end
-        tag.ChildBinding = pat.childbinding
-        tag.First = first
-
-        strngs2 = objectify.SubElement(tag, 'AnimatedGroups')
-        self.position = pat_start_pos + pat.secondsoffset
-        strngs2.Groupname = [ self.unpackkvarstr() for _ in range(pat.numseconds) ]
-
-        self.read_header()
-        
     def read_pai(self):
-        pai_start_pos = self.position - 4
-        pai = types.pai_tup(*self.unpackk('IHBBHHI'))
-
-        tag = objectify.SubElement(self.newroot, 'tag', type='pai1')
-        tag.attrib['framesize'] = str(pai.framesize)
-        tag.attrib['flags'] = str(pai.flags)
-
-        timgs_offsets = [ self.unpackk('I') for _ in range(pai.numtimgs) ]
-        for timgs_offset in timgs_offsets:
-            self.position = pai_start_pos + 20 + timgs_offset
-            timgs = objectify.SubElement(tag, 'timg', name=self.unpackkvarstr())
-
-        self.position = pai_start_pos + pai.entryoffset
-        pane_offsets = [ self.unpackk('I') for _ in range(pai.numentries) ]
-        for pane_offset in pane_offsets:
-            self.read_pane(pai_start_pos, pane_offset)
+        pai = dict()
+        pai['start_pos'] = self.position
+        pai['magic'] = self.unpackkstr(4)
+        pai['length'] = self.unpackk('I')
+        pai['framesize'] = self.unpackk('H')
+        pai['flags'] = self.unpackk('B')
+        pai['pad'] = self.unpackk('B')
+        pai['numtimgs'] = self.unpackk('H')
+        pai['numentries'] = self.unpackk('H')
+        pai['entryoffset'] = self.unpackk('I')
+        pai['timgs_offsets'] = [ self.unpackk('I') for _ in range(pai['numtimgs']) ]
+        pai['timgnames'] = []
+        for offset in pai['timgs_offsets']:
+            self.position = pai['start_pos'] + 20 + offset
+            pai['timgnames'].append(unpackkvarstr())
+        self.position = pai['start_pos'] + pai['entryoffset']
+        pai['pane_offsets'] = [ self.unpackk('I') for _ in range(pai['numentries']) ]
+        pai['panes'] = [ self.read_pane(pai['start_pos'], offset) for offset in pai['pane_offsets'] ]
+        return pai
 
     def read_pane(self, pai_start_pos, pane_offset):
         self.position = pai_start_pos + pane_offset
-        name = self.unpackkvarstr()
-        entry = types.entry_tup(*self.unpackk('BBH'))
-        pane = objectify.SubElement(tag, 'pane', name=name, type=str(entry.is_material))
-
-        tag_offsets = [ self.unpackk('I') for _ in range(entry.num_tags) ]
-        for tag_offset in tag_offsets:
-            self.read_tag(pai_start_pos, pane_offset, tag_offset)
+        pane = dict()
+        pane['name'] = self.unpackkvarstr()
+        pane['num_tags'] = self.unpackk('B')
+        pane['is_material'] = self.unpackk('B')
+        pane['pad'] = self.unpackk('H')
+        pane['tag_offsets'] = [ self.unpackk('I') for _ in range(pane['num_tags']) ]
+        pane['tags'] = [ self.read_tag(pai_start_pos, pane_offset, offset) for offset in pane['tag_offsets'] ]
+        return pane
 
     def read_tag(self, pai_start_pos, pane_offset, tag_offset):
         self.position = pai_start_pos + pane_offset + tag_offset
-        typetree = objectify.SubElement(pane, 'tag')
-        tag_start_pos = self.position
-        tag_type = self.unpackkstr(4)
-        # TODO Handle this more elegantly if possible.
-        if tag_type == ',\x00\x00\x00':
-            tag_type = self.unpackkstr(4)
-        typetree.attrib['type'] = tag_type
-        entry_count = self.unpackk('B')
-        pad = self.unpackk('3B')
-            
-        tag_data_offsets = [ self.unpackk('I') for _ in range(entry_count) ]
-        for tag_data_offset in tag_data_offsets:
-            self.read_tag_data(tag_start_pos, tag_data_offset)
+        tag = dict()
+        tag['start_pos'] = self.position
+        tag['type'] = self.unpackkstr(4)
+        if tag['type'] == ',\x00\x00\x00':
+            tag['type'] = self.unpackkstr(4)
+        tag['entry_count'] = self.unpackk('B')
+        tag['pad'] = self.unpackk('3B')
+        tag['data_offsets'] = [ self.unpackk('I') for _ in range(tag['entry_count']) ]
+        tag['datas'] = [ self.read_tag_data(tag, offset) for offset in tag['data_offsets'] ]
+        return tag
 
-    def read_tag_data(self, tag_start_pos, tag_data_offset):
-        self.position = tag_start_pos + tag_data_offset
-        tag_data = types.tag_data_tup(*self.unpackk('BBHHHI'))
+    def read_tag_data(self, tag, data_offset):
+        self.position = tag['start_pos'] + data_offset
+        data = dict()
+        data['type1'] = self.unpackk('B')
+        data['type2'] = self.unpackk('B')
+        data['type'] = self.unpackk('H')
+        data['coordcount'] = self.unpackk('H')
+        data['pad1'] = self.unpackk('H')
+        data['offsettotagdata'] = self.unpackk('I')
         try:
-            typename = types.typedict[tag_type][tag_data.type2]
+            data['typename'] = types.typedict[tag['type']][data['type2']]
         except KeyError:
-            typename = str(tag_data.type2)
+            data['typename'] = str(data['type2'])
+        data['content'] = []
+        if data['type'] == 2:
+            data['content'] = [ self.triplet() for _ in range(data['coordcount']) ]
+        elif data['type'] == 1:
+            data['content'] = [ self.pair() for _ in range(data['coordcount']) ]
+        return data
 
-        entry = objectify.SubElement(typetree, 'entry', type1=str(tag_data.type1), type2=typename)
-        if tag_data.datatype == 2:
-            self.triplet(tag_data.coordcount, entry)
-        elif tag_data.datatype == 1:
-            self.pair(tag_data.coordcount, entry)
-    
-    def triplet(self, count, entry):
-        for _ in range(count):
-            info = objectify.SubElement(entry, 'triplet')
-            info.frame, info.value, info.blend = self.unpackk('3f')
+    def triplet(self):
+        info = objectify.Element('triplet')
+        info.frame, info.value, info.blend = self.unpackk('3f')
+        return info
 
-    def pair(self, count, entry):
-        for _ in range(count):
-            info = objectify.SubElement(entry, 'pair')
-            info.frame, info.data2, info.padding = self.unpackk('fHH')
+    def pair(self):
+        info = objectify.Element('pair')
+        info.frame, info.data2, info.padding = self.unpackk('fHH')
+        return info
--- include/types.py
+++ include/types.py
@@ -1,16 +1,10 @@
-from collections import namedtuple
-
 typedict = {
-    'FLPA': ["Xtrans", "Ytrans", "Ztrans", "Xrotate", "Yrotate", "Zrotate", "Xscale", "Yscale", "Xsize", "Ysize"],
-    'FLVI': ["visible"],
-    'FLTP': ["TexturePattern"],
-    'FLVC': ["LT_red", "LT_green", "LT_blue", "LT_alpha", "RT_red", "RT_green", "RT_blue", "RT_alpha", "LB_red", "LB_green", "LB_blue", "LB_alpha", "RB_red", "RB_green", "RB_blue", "RB_alpha", "PaneAlpha"],
-    'FLMC': ["BlackColor_red", "BlackColor_green", "BlackColor_blue", "BlackColor_alpha", "WhiteColor_red", "WhiteColor_green", "WhiteColor_blue", "WhiteColor_alpha"],
-    'FLTS': ["Utrans", "Vtrans", "rotate", "Uscale", "Vscale"],
-    'FLIM': ["rotate", "Uscale", "Vscale"]
+    'FLPA': ['Xtrans', 'Ytrans', 'Ztrans', 'Xrotate', 'Yrotate', 'Zrotate', 'Xscale', 'Yscale', 'Xsize', 'Ysize'],
+    'FLVI': ['visible'],
+    'FLTP': ['TexturePattern'],
+    'FLVC': ['LT_red', 'LT_green', 'LT_blue', 'LT_alpha', 'RT_red', 'RT_green', 'RT_blue', 'RT_alpha', 'LB_red', 'LB_green', 'LB_blue', 'LB_alpha', 'RB_red', 'RB_green', 'RB_blue', 'RB_alpha', 'PaneAlpha'],
+    'FLMC': ['BlackColor_red', 'BlackColor_green', 'BlackColor_blue', 'BlackColor_alpha', 'WhiteColor_red', 'WhiteColor_green', 'WhiteColor_blue', 'WhiteColor_alpha'],
+    'FLTS': ['Utrans', 'Vtrans', 'rotate', 'Uscale', 'Vscale'],
+    'FLIM': ['rotate', 'Uscale', 'Vscale'],
+    'FLEU': ['UNKNOWN']
 }
-bflanheader_tup = namedtuple('bflan', ['endian', 'firstsectionoffsetree', 'version', 'pad1', 'filesize', 'sections', 'pad2'])
-pat_tup = namedtuple('pat1', ['length', 'animorder', 'numseconds', 'firstoffset', 'secondsoffset', 'start', 'end', 'childbinding', 'pad', 'pad1'])
-pai_tup = namedtuple('pai1', ['length', 'framesize', 'flags', 'pad', 'numtimgs', 'numentries', 'entryoffset'])
-entry_tup = namedtuple('entry', ['num_tags', 'is_material', 'pad'])
-tag_data_tup = namedtuple('tag_data', ['type1', 'type2', 'datatype', 'coordcount', 'pad1', 'offsettotagdata'])
